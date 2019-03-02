fb4ced1bf36dc56d59665c8219f7ed26b10ab720
Administrator <1145265355@qq.com>
2019-03-02 20:06:36 +0800

    asss

---
+++ src/main/java/cn/zyz/springbootredis/Book.java
@@ -0,0 +1,12 @@
+package cn.zyz.springbootredis;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Book implements Serializable{
+    private Integer id;
+    private String name;
+    private String author;
+}
